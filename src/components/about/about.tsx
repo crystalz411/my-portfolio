c6a202a323da36ce2f471c49a0570dbfeb351720
Crystal Yalanopoulos <crystal.yalanopoulos@validusrm.com>
2022-04-12 18:39:34 +0100

    about page created

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -1,9 +1,51 @@
 import React from 'react'
+import me from '../../assets/me-about.jpg'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
 import './about.css'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className='container about_container'>
+        <div className='about_me'>
+          <div className='about_me-image'>
+            <img src={me} alt="About Image" />
+          </div>
+        </div>
+        <div className='about_content'>
+          <div className="about_cards">
+            <article className='about_card'>
+              <FaAward className='about_icon'/>
+              <h5>Experience</h5>
+              <small>1+ years of experience</small>
+            </article>
+
+            <article className='about_card'>
+              <FiUsers className='about_icon'/>
+              <h5>Clients</h5>
+              <small>something here</small>
+            </article>
+
+            <article className='about_card'>
+              <VscFolderLibrary className='about_icon'/>
+              <h5>Projects</h5>
+              <small>3+ projects</small>
+            </article>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Qui amet voluptatum officiis cum error optio libero cupiditate rerum quas. 
+            In eius illum possimus. Ipsam explicabo, assumenda deleniti blanditiis omnis unde.
+          </p>
+          <a href='#contact' className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -11,8 +11,8 @@ const Header = () => {
             <h5> Helo I'm </h5>
             <h1>Crystal Yal</h1>
             <h5 className="text-light">Frontend Developer</h5>
-            {/* <HeaderSocials />
-            <CTA /> */}
+            <HeaderSocials />
+            <CTA />
             <div className="me">
                 <img src={me}/>
             </div>
