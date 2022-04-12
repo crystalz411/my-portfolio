5a39706595ce459af0efa2f7a7b2a6055cb131cb
Crystal Yalanopoulos <crystal.yalanopoulos@validusrm.com>
2022-04-12 18:51:18 +0100

    about section created

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -1,11 +1,39 @@
 import React from 'react'
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {BiBook} from 'react-icons/bi'
+import {RiServiceLine} from 'react-icons/ri'
+import {BiMessageSquareDots} from 'react-icons/bi'
+import { useState } from 'react'
 import './nav.css'
 
 const Nav = () => {
+    const [activeNav, setActiveNav] = useState('#')
     return (
-        <div>
-            Nav
-        </div>
+        <nav>
+            <a href="#" 
+            className={activeNav === '#' ? 'active' : ''}>
+                <AiOutlineHome /></a>
+    
+            <a href="#about" 
+            className={activeNav === '#about' ? 'active' : ''}
+            onClick={() => setActiveNav('#about')}>
+                <AiOutlineUser /></a>
+
+            <a href="#experience"
+            className={activeNav === '#experience' ? 'active' : ''}
+            onClick={() => setActiveNav('#experience')}>
+                <BiBook /></a>
+
+            <a href="#services"
+            className={activeNav === '#services' ? 'active' : ''}
+            onClick={() => setActiveNav('#services')}>
+                <RiServiceLine /></a>
+
+            <a href="#contact" className={activeNav === '#contact' ? 'active' : ''}
+            onClick={() => setActiveNav('#contact')}>
+                <BiMessageSquareDots /></a>
+        </nav>
     )
 
 }
